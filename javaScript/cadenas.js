a66ee63e408e0a04853e1f587f394c7236d13e61
Anthony Tamayo Ortega <anthonytamayoortega@MacBook-Pro-de-Anthony.local>
2017-07-08 11:04:13 -0500

    Manejo de cadenas o astringís terminado

---
+++ javaScript/cadenas.js
@@ -0,0 +1,6 @@
+//Manejo de cadenas en JavaScript
+var mensaje;
+mensaje="Bienvenido al curso de \"JavaScript\"\n\nEsperamos que te diviertas ";
+var codPos= "54054";
+var clave = "";
+alert(mensaje);
